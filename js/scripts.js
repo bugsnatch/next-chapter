1f73ce7fe8285f1e43dc6d5fc7339aaaa2d0abbd
Stefano Pecorino <stefano.pecorino@leaseplan.com>
2024-06-22 13:41:50 +0200

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -208,14 +208,16 @@ $(document).ready(function () {
 
 
     /********************** RSVP **********************/
+    $('#invite_code').val(inviteCode);
+
     $('#rsvp-form').on('submit', function (e) {
         e.preventDefault();
         var data = $(this).serialize();
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== '518feb178523d3c70ef61b57bbe176c4')
+        {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
             $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
